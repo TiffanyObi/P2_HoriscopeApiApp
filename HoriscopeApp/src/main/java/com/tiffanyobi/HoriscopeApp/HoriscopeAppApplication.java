830cd20f24d99a156118b4034a087d6e3af33332
Tiffany Obi <tiffanyobi123@gmail.com>
2022-02-15 22:34:37 -0500

    pushing up spring boot project made from the spring initializer

---
+++ HoriscopeApp/src/main/java/com/tiffanyobi/HoriscopeApp/HoriscopeAppApplication.java
@@ -0,0 +1,13 @@
+package com.tiffanyobi.HoriscopeApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoriscopeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoriscopeAppApplication.class, args);
+	}
+
+}
---
+++ HoriscopeApp/src/test/java/com/tiffanyobi/HoriscopeApp/HoriscopeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiffanyobi.HoriscopeApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoriscopeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
